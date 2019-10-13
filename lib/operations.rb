5502b9fda6dab793b13f8cab9f483eb4c9492a19
Thomas Wright <twwright@gmail.com>
2019-10-13 19:43:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
   if 40 > speed || speed < 60
-    "true"
+    true
   else
-    "false"
+    false
   end
 end
 
 
 
 def not_safe?(speed)
-  40 > speed || speed < 60 ? "true" : "false"
+  40 > speed || speed < 60 ? true : false
 end
